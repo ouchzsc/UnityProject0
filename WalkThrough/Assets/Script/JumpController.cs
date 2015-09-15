d980783acbbe259fdb09de89af7b3b12df6cf9ed
ouchzsc <ouchzsc@gmail.com>
2015-09-16 01:55:26 +0800

    First Editon
    Add to Cloud Repository

---
+++ WalkThrough/Assets/Script/JumpController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpController : MonoBehaviour {
+
+    public float JumpForce;
+
+    public Transform FeetPoint;
+    public LayerMask WhatIsGround;
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = transform.GetComponent<Rigidbody2D>();
+        animator = transform.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("VerticalSpeed", rigidbody.velocity.y);
+        if (Physics2D.OverlapPoint(FeetPoint.position, WhatIsGround))
+        {
+            animator.SetBool("InAir", false);
+            if (Input.GetButtonDown("Jump"))
+            {
+                rigidbody.AddForce(new Vector2(0,JumpForce));
+                print("jump");
+            }
+        }
+        else animator.SetBool("InAir", true);
+        
+	}
+}
---
+++ WalkThrough/Assets/Script/MoveController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    public Transform FeetPoint;
+    public LayerMask WhatIsGround;
+    public float WalkSpeed = 1f;
+    public float RunSpeed = 4f;
+
+    private float Horizontal;
+    private float RealSpeed;
+    private float WalkPressedLogTime;
+    
+    new Rigidbody2D rigidbody;
+    Animator animator;
+
+	void Start () {
+        rigidbody = transform.GetComponent<Rigidbody2D>();
+        animator = transform.GetComponent<Animator>();
+	}
+	
+	void Update () {
+        if (!Physics2D.OverlapPoint(FeetPoint.position, WhatIsGround))
+            return;
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        if (Horizontal != 0)
+            transform.localScale = new Vector3(Horizontal, 1, 1);
+        RealSpeed = RunSpeed * Horizontal;
+        if (Input.GetButton("Down"))        
+            RealSpeed = WalkSpeed * Horizontal;       
+            
+        rigidbody.velocity = new Vector2(RealSpeed, rigidbody.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(RealSpeed));
+	}
+
+}
