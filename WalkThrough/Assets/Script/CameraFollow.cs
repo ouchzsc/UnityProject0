f13124ad9ea2efc490c46aeb2979e3790821271e
ouchzsc <ouchzsc@gmail.com>
2015-09-18 20:22:28 +0800

    camera
    11

---
+++ WalkThrough/Assets/Script/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+    public float LineHeight;
+    public Color LineColor;
+    public Transform []WinLine;
+    
+    void Start()
+    {
+    }
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = LineColor;
+        foreach (var item in WinLine)
+        {            
+            Gizmos.DrawLine(new Vector3(item.position.x, item.position.y + LineHeight, item.position.z), new Vector3(item.position.x, item.position.y - LineHeight, item.position.z));
+        }
+
+    }
+
+    
+}
